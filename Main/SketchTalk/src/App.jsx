f7c2833c3c459db60d6636d9f2c2d18c5667a7ce
Saurabh209 <saurabhhh2613@gmail.com>
2024-11-25 14:12:52 +0530

    tailwind setup done

--- Main/SketchTalk/src/App.jsx
+++ Main/SketchTalk/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./output.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h3> put these foolish ambitions to rest</h3>
+      <h3 className="bg-orange-300"> put these foolish ambitions to rest</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
