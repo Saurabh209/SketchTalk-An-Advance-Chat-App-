8c3b31a3f2892c672d001738052dce270a2b184b
Saurabh209 <saurabhhh2613@gmail.com>
2024-11-25 14:29:37 +0530

    few changes

--- Main/SketchTalk/src/App.jsx
+++ Main/SketchTalk/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./output.css";
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h3 className="bg-orange-300"> put these foolish ambitions to rest</h3>
+      <h3 className="bg-pink-700"> put these foolish ambitions to rest</h3>
     </>
   );
 }
