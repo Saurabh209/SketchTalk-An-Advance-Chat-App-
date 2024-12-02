58fb48a1f6718b393c3fbc74f47b863f36224011
Saurabh209 <saurabhhh2613@gmail.com>
2024-12-02 20:49:56 +0530

    new assets added

--- Main/SketchTalk/src/Components/homePage/body/Body.jsx
+++ Main/SketchTalk/src/Components/homePage/body/Body.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Body() {
     return (
         <div className="flex-1 p-8">
-            <h1 className='text-2xl text-center font-bold text-[#323232]'>Home</h1>
+            <h1 className='text-2xl text-center font-bold text-[#000000]'>Home</h1>
         </div>
     )
 }
