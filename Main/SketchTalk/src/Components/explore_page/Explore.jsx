26833126230221a05c77a09704f1dc793c54150a
Saurabh209 <saurabhhh2613@gmail.com>
2024-12-04 15:41:06 +0530

    new roadmap added

--- Main/SketchTalk/src/Components/explore_page/Explore.jsx
+++ Main/SketchTalk/src/Components/explore_page/Explore.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Explore() {
-    return (
-        <div className='flex-1 p-8'>
-            <h1 className='text-2xl text-center font-bold text-[#323232]'>Explore</h1>
-        </div>
-    )
+  return (
+    <div className="flex-1 p-8">
+      <h1 className="text-2xl text-center font-bold text-[#323232]">
+        Welcome to SketchTalk
+      </h1>
+
+      <div className="border-2 border-[#323232] rounded-md w-[100%] h-[100%]"></div>
+    </div>
+  );
 }
 
-export default Explore
+export default Explore;
